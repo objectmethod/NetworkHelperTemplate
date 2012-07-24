4a36812cc0656baad9c15174cc0e95e61a4f3e7a
Richard Guy <github@richardpaulguy.com>
2012-07-24 15:52:37 -0400

    removing files

--- NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/BaseViewController.h
+++
@@ -1,8 +0,0 @@
-@interface BaseViewController : UIViewController
-
-- (void) getDataFromUrl:(NSURL*)url;
-
-- (void) receivedResponse:(id)response fromUrl:(NSURL*)url;
-- (void) receivedError:(NSError*)error fromUrl:(NSURL*)url;
-
-@end
