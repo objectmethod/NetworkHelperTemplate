c921d2e69697553ee951ff3805ef96f566ab643a
Richard Guy <github@richardpaulguy.com>
2012-07-24 15:50:20 -0400

    removing files

--- NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/NetworkTest/BaseTableViewController.h
+++
@@ -1,7 +0,0 @@
-#import "BaseViewController.h"
-
-@interface BaseTableViewController : BaseViewController<UITableViewDelegate, UITableViewDataSource>
-
-@property (strong, nonatomic) UITableView *tableView;
-
-@end
